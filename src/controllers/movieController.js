c3fd72264207fbd61c65141c884e1c7567ef8973
Martin Marinov <martin.marinov10@yahoo.com>
2025-05-23 19:27:48 +0100

    Add body parser

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,4 +6,12 @@ movieController.get('/create', (req, res) => {
     res.render('create');
 });
 
+movieController.post('/create', (req, res) => {
+    const newMovie = req.body;
+
+    // Save movie
+
+    // Redirect to home page
+});
+
 export default movieController;
