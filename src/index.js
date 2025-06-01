8cbf6ff7ec63b8fcdfda3e74b8fcd9dedb5963cb
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-01 12:46:19 +0100

    Add mongoose and connect to the DB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import mongoose from 'mongoose';
 
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
@@ -22,6 +23,16 @@ app.engine('hbs', handlebars.engine({
         }
     }
 }));
+
+// Connect database
+try {
+   await mongoose.connect(`mongodb://localhost:27017`, { dbName: 'magic-movies-workshop' });
+   console.log('Successfully connected!');
+   
+} catch (err) {
+    console.log('Cannot connect to DB!');
+    console.log(err.message);
+}
 
 // Set default engine
 app.set('view engine', 'hbs');
