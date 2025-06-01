b5b7970f5d31795d668ecd8ab8efdc5d0be2d733
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-01 19:35:02 +0100

    Add new Cast model

---
+++ src/models/Cast.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const castSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+    },
+    age: {
+        type: Number,
+        required: [true, 'Age is required!'],
+        min: [5, 'Age must be at least 5!'],
+        max: [120, 'Age must be no more than 120!'],
+    },
+    born: {
+        type: String,
+        required: true,  // TODO: ADD MESSAGE 
+    },      
+    imageUrl: {
+        type: String,
+        required: [true, 'Image is required!'],
+        validate: [/^https?:\/\//, 'Invalid Image URL!'],
+    }
+});
+
+const Cast = model('Cast', castSchema);
+
+export default Cast;
--- src/models/Movie.js
+++ src/models/Movie.js
@@ -5,41 +5,41 @@ const maxYearAllowed = new Date().getFullYear() + 5;
 const movieSchema = new Schema({
     title: {
         type: String,
-        required: [true, 'Title is required!']
+        required: [true, 'Title is required!'],
     },
     category: {
         type: String,
-        required: [true, 'Category is required!']
+        required: [true, 'Category is required!'],
     },
     genre: {
         type: String,
-        required: [true, 'Genre is required!']
+        required: [true, 'Genre is required!'],
     },
     director: {
         type: String,
-        required: [true, 'Director is required!']
+        required: [true, 'Director is required!'],
     },
     year: {
         type: Number,
         required: [true, 'Year is required!'],
-        min: [1970, "Year should be equal or greater than 1970"],
-        max: [maxYearAllowed, `Year cannot be larget than ${maxYearAllowed}`]
+        min: [1970, "Year should be equal or greater than 1970!"],
+        max: [maxYearAllowed, `Year cannot be larget than ${maxYearAllowed}!`],
     },
     imageUrl: {
         type: String,
         required: [true, 'Image is required!'],
-        validate: [/^https?:\/\//, 'Invalid Image URL!']
+        validate: [/^https?:\/\//, 'Invalid Image URL!'],
     },
     rating: {
         type: Number,
         required: [true, 'Rating is required!'],
-        min: [1, 'Rating must be 1 or higher.'],
-        max: [10, 'Rating must be 10 or lower.']
+        min: [1, 'Rating must be 1 or higher!'],
+        max: [10, 'Rating must be 10 or lower!'],
     },
     description: {
         type: String,
         required: [true, 'Description is required!'],
-        maxLength: [800, 'Description is too long!']
+        maxLength: [800, 'Description is too long!'],
     }
 });
 
