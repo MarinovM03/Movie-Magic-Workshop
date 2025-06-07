a1ada2539297634f5d8dee8caf37dbc1f231971f
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-07 14:53:28 +0100

    Register user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import userService from '../services/userService.js';
 
 const userController = Router();
 
 //TODO: Add actions
 userController.get('/register', (req, res) => {
     res.render('user/register'); 
+});
+
+userController.post('/register', async (req, res) => {
+    const userData = req.body;
+    
+    // Register user
+    await userService.register(userData);
+
+    res.redirect('user/login');
 });
 
 export default userController;
